73ee4486ca673dca564a0fc2ee7fae74f4156890
Naym0 <nmuswanya@gmail.com>
2018-10-29 23:12:47 +0300

    Typo correction.

--- transaction-mgmt.php
+++ transaction-mgmt.php
@@ -28,7 +28,7 @@
                 <!--Menu Trigger on Small Screen-->
                 <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
                 <ul id="nav-mobile" class="right hide-on-small-only">  
-                    <li><a class="dropdown-trigger user-name" href="#" data-target="dropdown1"><?echo $_SESSION['FName'][0]?><i class="material-icons right">arrow_drop_down</i></a></li>
+                    <li><a class="dropdown-trigger user-name" href="#" data-target="dropdown1"><?php echo $_SESSION['FName'][0]?><i class="material-icons right">arrow_drop_down</i></a></li>
                 </ul>
             </div>
         </nav>
